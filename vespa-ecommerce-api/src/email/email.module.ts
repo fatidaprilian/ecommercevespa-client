879ff87fd27f4291fe5631a04e4abd217b14581f
fatidaprilian <faridaprilian214@gmail.com>
2025-08-23 17:31:30 +0700

    Initial commit

--- vespa-ecommerce-api/src/email/email.module.ts
+++ vespa-ecommerce-api/src/email/email.module.ts
@@ -5,6 +5,6 @@ import { EmailService } from './email.service';
 
 @Module({
   providers: [EmailService],
-  exports: [EmailService], // Ini memastikan service bisa di-'inject' di modul lain
+  exports: [EmailService],
 })
 export class EmailModule {}
