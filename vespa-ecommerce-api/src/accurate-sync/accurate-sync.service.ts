88771fb52218274d0f1c4bf163ad24d5a6c775ba
fatidaprilian <faridaprilian214@gmail.com>
2025-09-22 17:17:23 +0700

    LAST

--- vespa-ecommerce-api/src/accurate-sync/accurate-sync.service.ts
+++ vespa-ecommerce-api/src/accurate-sync/accurate-sync.service.ts
@@ -114,7 +114,7 @@ export class AccurateSyncService {
         }
     }
     
-    @Cron(CronExpression.EVERY_DAY_AT_2AM)
+    @Cron(CronExpression.EVERY_MINUTE)
     async scheduleWebhookRenewal() {
         this.logger.log('CRON JOB: Triggering Accurate webhook renewal.');
         await this.accurateService.renewWebhook();
